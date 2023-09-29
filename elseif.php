b3c5a5b8be6f9be86e1fb132f143c3eb2a4a1867
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-09-29 10:40:10 -0300

    commit bia

--- elseif.php
+++ elseif.php
@@ -9,8 +9,8 @@
 
     <?php
 // Declare duas variáveis com números e uma com uma string
-$numero1 = 42;
-$numero2 = 75;
+$numero1 = 50;
+$numero2 = 60;
 $texto = "É uma string";
 
     if (is_numeric($numero1)) {
@@ -19,7 +19,7 @@ $texto = "É uma string";
 
     if ($resultado1 > 100) {
         
-        echo "O resultado é maior que 100: $resultado1<br>";
+        echo "O número é maior que 100: $resultado1<br>";
     }
     } else {
         echo "A variável não é um número<br>";
@@ -28,14 +28,14 @@ $texto = "É uma string";
     if (is_numeric($numero2)) {
     $resultado2 = $numero2 * 2;
     if ($resultado2 > 100) {
-        echo "O resultado é maior que 100: $resultado2<br>";
+        echo "O número é maior que 100: $resultado2<br>";
     }
     }
 
     if (is_numeric($texto)) {
     $resultado3 = $texto * 2;
     if ($resultado3 > 100) {
-        echo "O resultado é maior que 100: $resultado3<br>";
+        echo "O número é maior que 100: $resultado3<br>";
     }
     } 
     ?>
